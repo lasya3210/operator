3fc92086c98f8e3929616bba92bcce60e944d363
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:34:52 +0530

    Create ranchi.py

---
+++ ranchi.py
@@ -0,0 +1,12 @@
+...
+//input
+First line of input consists of one integer value as age of Dhoni.
+//output
+  Output should display an integer that specifies Devki Devi's current age
+  //sample input and output 1
+  6
+33
+
+  //sample input and output 2
+  3
+21
